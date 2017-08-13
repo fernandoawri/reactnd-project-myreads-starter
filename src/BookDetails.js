c0249611e61b044fd16b95cf8044ad3ed544288f
Fernando Benavides <fernandoawri@gmail.com>
2017-08-13 16:49:44 -0400

    Created BookDetails Component
    * New BookDetails Component added to display the book’s information

---
+++ src/BookDetails.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class BookDetails extends Component {
+  render(){
+    const { book } = this.props
+    return(
+      <div className="list-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              value="Book Details"
+              className="book-details-top-title"
+              disabled
+            />
+          </div>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {book.title && (
+              <li key={book.id} >
+                <div className="book-details">
+                  <h1>{book.title}</h1>
+                  <h2 className="book-authors">{book.authors.map((author) => `[${author}] `)}</h2>
+                  <div className="book-content-details">
+                    <a href={book.previewLink} target="_blank">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks !== undefined? book.imageLinks.thumbnail:''}")` }}></div>
+                      <p>Preview</p>
+                    </a>
+                    <div className="content-details">
+                      <p><b>Shelf:</b> {book.shelf && (book.shelf)}</p>
+                      <p><b>Categories:</b> {book.categories && (book.categories.map((category) => `[${category}] `))}</p>
+                      <p><b>Publisher:</b> {book.publisher}</p>
+                      <p><b>Published Date:</b> {book.publishedDate}</p>
+                      <p><b>Pages:</b> {book.pageCount}</p>
+                      <p><b>Language:</b> {book.language}</p>
+                      <p><b>Description:</b> {book.description}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookDetails
