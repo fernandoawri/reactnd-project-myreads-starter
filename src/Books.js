f0187f8a84d34f84a920b4217dccd581da6ded54
Fernando Benavides <fernandoawri@gmail.com>
2017-08-14 10:35:44 -0400

    Fix: Validate book.authors
    * Added validation for book.authors array

--- src/Books.js
+++ src/Books.js
@@ -24,6 +24,7 @@ class Books extends Component {
                     </div>
                   </div>
                   <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.authors ? book.authors.join(', '): ''}</div>
                 </div>
               </li>
             ))}
