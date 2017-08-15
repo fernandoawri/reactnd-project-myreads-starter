77c33e5d1c6f203fa0f3fbb5a8010556a55b91a1
Fernando Benavides <fernandoawri@gmail.com>
2017-08-14 21:47:07 -0400

    Update: BookDetails - BookDetailed integration
    * Added BookDetailed Component

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import BookDetailed from './BookDetailed'
 import { Link } from 'react-router-dom'
 
 class BookDetails extends Component {
@@ -13,31 +14,7 @@ class BookDetails extends Component {
           </div>
         </div>
         <div className="list-books-content">
-          <div>
-            {book.title && (
-              <li key={book.id} >
-                <div className="book-details">
-                  <h1>{book.title}</h1>
-                  <h2 className="book-authors">{book.authors.map((author) => `[${author}] `)}</h2>
-                  <div className="book-content-details">
-                    <a href={book.previewLink} target="_blank">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks !== undefined? book.imageLinks.thumbnail:''}")` }}></div>
-                      <p>Preview</p>
-                    </a>
-                    <div className="content-details">
-                      <p><b>Shelf:</b> {book.shelf && (book.shelf)}</p>
-                      <p><b>Categories:</b> {book.categories && (book.categories.map((category) => `[${category}] `))}</p>
-                      <p><b>Publisher:</b> {book.publisher}</p>
-                      <p><b>Published Date:</b> {book.publishedDate}</p>
-                      <p><b>Pages:</b> {book.pageCount}</p>
-                      <p><b>Language:</b> {book.language}</p>
-                      <p><b>Description:</b> {book.description}</p>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            )}
-          </div>
+          <BookDetailed book={book} />
         </div>
         <div className="open-search">
           <Link to="/search" className="add-contact">Add a book</Link>
