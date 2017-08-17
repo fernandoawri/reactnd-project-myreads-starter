959c8cb293e8eab0b412e5937ecbe35ec2c8c3a8
Fernando Benavides <fernandoawri@gmail.com>
2017-08-16 23:14:07 -0400

    Update: change to stateless component.
    * This component should be a stateless component.

--- src/BookDetailed.js
+++ src/BookDetailed.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class BookDetailed extends Component {
-  render(){
-    const { book} = this.props
-    return(
-      <div>
-        {book.title && (
-          <li key={book.id} >
-            <div className="book-details">
-              <h1>{book.title}</h1>
-              <h2 className="book-authors">{book.authors ? book.authors.join(', '): ''}</h2>
-              <div className="book-content-details">
-                <a href={book.previewLink} target="_blank">
-                  <div className="book-cover" style={{backgroundImage: `url("${book.imageLinks !== undefined? book.imageLinks.thumbnail:''}")` }}></div>
-                  <p>Preview</p>
-                </a>
-                <div className="content-details">
-                  <p><b>Shelf:</b> {book.shelf && (book.shelf)}</p>
-                  <p><b>Categories:</b> {book.categories ? book.categories.join(', '): ''}</p>
-                  <p><b>Publisher:</b> {book.publisher}</p>
-                  <p><b>Published Date:</b> {book.publishedDate}</p>
-                  <p><b>Pages:</b> {book.pageCount}</p>
-                  <p><b>Language:</b> {book.language}</p>
-                  <p><b>Description:</b> {book.description}</p>
-                </div>
+const BookDetailed = (props) => {
+  const book = props.book
+  return(
+    <div>
+      {book.title && (
+        <li key={book.id} >
+          <div className="book-details">
+            <h1>{book.title}</h1>
+            <h2 className="book-authors">{book.authors ? book.authors.join(', '): ''}</h2>
+            <div className="book-content-details">
+              <a href={book.previewLink} target="_blank">
+                <div className="book-cover" style={{backgroundImage: `url("${book.imageLinks !== undefined? book.imageLinks.thumbnail:''}")` }}></div>
+                <p>Preview</p>
+              </a>
+              <div className="content-details">
+                <p><strong>Shelf:</strong> {book.shelf && (book.shelf)}</p>
+                <p><strong>Categories:</strong> {book.categories ? book.categories.join(', '): ''}</p>
+                <p><strong>Publisher:</strong> {book.publisher}</p>
+                <p><strong>Published Date:</strong> {book.publishedDate}</p>
+                <p><strong>Pages:</strong> {book.pageCount}</p>
+                <p><strong>Language:</strong> {book.language}</p>
+                <p><strong>Description:</strong> {book.description}</p>
               </div>
             </div>
-          </li>
-        )}
-      </div>
-    )
-  }
+          </div>
+        </li>
+      )}
+    </div>
+  )
 }
 
 export default BookDetailed
